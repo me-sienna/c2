7cfe4e174d8f4537bd90c160703d16f98262d358
me-sienna <144670217+me-sienna@users.noreply.github.com>
2024-04-12 16:31:37 +0900

    Update 멘토링 3주차 과제1_고명은.c

--- 멘토링 3주차 과제1_고명은.c
+++ 멘토링 3주차 과제1_고명은.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
+	int i;
+	for (i = 33; i < 127; i++) {
+		printf("%d %x %c", i, i, i);
+	}
+	return 0;
+}
+
+
+
+#include <stdio.h>
+
+int main(void) {
 	int i,a,b,c;
 	int arr[30][6];
 
@@ -16,16 +28,6 @@ int main(void) {
 		for (b = 0; b < 6; b++) {
 			printf("%d %x %c ", arr[a][b], arr[a][b], arr[a][b]);
 		}
-	}
-	return 0;
-}
-
-#include <stdio.h>
-
-int main(void) {
-	int i;
-	for (i = 33; i < 127; i++) {
-		printf("%d %x %c", i, i, i);
 	}
 	return 0;
 }
